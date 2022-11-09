573504e23e1efd2bdb0b048f1b25aa527cdab490
jiblett1000 <get@christianburkh.art>
2022-11-08 18:29:58 -0800

    remove dist/

--- dist/server/content-types/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const newsletter_1 = __importDefault(require("./newsletter"));
-const subscribed_user_1 = __importDefault(require("./subscribed-user"));
-exports.default = { newsletter: newsletter_1.default, "subscribed-user": subscribed_user_1.default };
